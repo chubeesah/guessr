78aefcce561418b1e3fdb94af1dc79982b10b84d
Chubes <Chubes@andrew-mercers-macbook.local>
2015-01-20 21:09:21 -0500

    Is this right?

--- lib/guessr.rb
+++ lib/guessr.rb
@@ -6,6 +6,11 @@ Camping.goes :Guessr
 
 module Guessr
   module Models
+
+    class NumberGuessingGame < Base
+      validates :answer, presence: true
+    end
+
     class Player < Base
       validates :name, presence: true, uniqueness: true
       # alternately: validates :name, presence: true
@@ -13,7 +18,7 @@ module Guessr
 
     class Hangman < Base
       validates :answer, presence: true,
-        format: { with: /^[a-z]+$/, message: "only lowercase words allowed"}
+        format: { with: /\A[a-z]+\z/, message: "only lowercase words allowed"}
       serialize :guesses
       before_update :set_finished!
 
@@ -46,72 +51,32 @@ module Guessr
           t.boolean :finished
           t.timestamps
         end
+
+        create_table NumberGuessingGame.table_name do |t|
+          t.integer :answer
+          t.integer :guesses
+          t.boolean :finished
+          t.timestamps
+        end
       end
 
       def self.down
         drop_table Player.table_name
         drop_table Hangman.table_name
+        drop_table NumberGuessingGame.table_name
+      end
+    end
+
+    class AddLimitToAnswer < V 1.1
+      def self.up
+        add_column NumberGuessingGame.table_name :answer_limit, :integer
+      end
+        
+      def self.down
+        remove_column NumberGuessingGame.table_name :answer_limit
       end
     end
   end
 end
 
-def Guessr.create
-  Guessr::Models.create_schema
-end
-
-def choose_player
-  # TODO
-end
-
-def new_player
-  # TODO
-end
-
-def resume_game
-  # TODO
-end
-
-def new_game
-  # TODO
-end
-
-def prompt(question, validator, error_msg, clear: nil)
-  `clear` if clear
-  puts "\n#{question}\n"
-  result = gets.chomp
-  until result =~ validator
-    puts "\n#{error_msg}\n"
-    result = gets.chomp
-  end
-  exit if result == 'QUIT'
-  puts
-  result
-end
-
-def player_screen
-  message = "Would you like to (1) pick an existing player, (2) add a new player, or (QUIT)?"
-  choice = prompt(message, /^([12]|QUIT)$/, "Please choose 1, 2, 3, or QUIT.", clear: true).to_i
-  case choice
-  when 1
-    choose_player
-  when 2
-    new_player
-  end
-end
-
-def game_screen
-  message = "Would you like to (1) continue an existing game, (2) start a new game, or (QUIT)?"
-  choice = prompt(message, /^([12]|QUIT)$/, "Please choose 1, 2, or QUIT.", clear: true).to_i
-  case choice
-  when 1
-    resume_game
-  when 2
-    new_game
-  end
-end
-
-puts "Welcome to Guessr!"
-player_screen
-game_screen
-puts "Thanks for playing!"
+  
